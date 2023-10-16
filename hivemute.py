5a9cc5f897904164b0c2be86698061f0ddc3c17f
bozzj <46000232+bozzj@users.noreply.github.com>
2023-10-16 15:00:37 -0400

    Create hivemute.py
    Python program accepts an account as input and then outputs the accounts that are muting the input account.

---
+++ hivemute.py
@@ -0,0 +1,15 @@
+from beem.account import Account
+from beem import Hive
+from beem.nodelist import NodeList
+nodelist = NodeList()
+nodelist.update_nodes()
+hive = Hive()
+print()
+print ('Please input a Hive username:')
+acc = str(input())
+account = Account(acc, blockchain_instance=hive)
+print()
+
+print("accounts muting", acc, ":")
+print(account.get_muters(raw_name_list=True, limit=100))
+print()
